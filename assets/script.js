43746de0dcf75827c8191962032fe31f737b62f0
Ioana Iosub <stu.waltham@gmail.com>
2024-01-14 17:27:40 +0000

    html page rafactored

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+const apiKey = "04bde8cc7f569f7c5603cdbc6deb89a3"; //API Key
+const defaultCity = "London"; // First City Displayed
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]; // An array with days of the week
