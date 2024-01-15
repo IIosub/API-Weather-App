5e294fa5538695547fcd2ffcfae588aa732dbd9a
Ioana Iosub <stu.waltham@gmail.com>
2024-01-15 20:32:46 +0000

    Html ids selected to display the data

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
-const apiKey = "f3e51c69dcc0d385851ded2f71199df2"; //API Key
-const defaultCity = "London"; // First City Displayed
-
+const apiKey = "f3e51c69dcc0d385851ded2f71199df2"; // API Key
 const days = [
   "Sunday",
   "Monday",
@@ -11,15 +9,31 @@ const days = [
   "Saturday",
 ]; // An array with days of the week
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${apiKey}&units=metric`;
-
-function randerWeather() {}
-
-//Fetch Weather data from the api for the city
-function fetchWeather(query) {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+function displayWeather(data) {
+  const displayCity = document.getElementById("city");
+  const temperature = document.getElementById("temp");
+  const wind = document.getElementById("wind");
+  const humidity = document.getElementById("humidity");
 }
 
+// Fetch Weather data from the API for the city
+function fetchWeather() {
+  const city = document.getElementById("search-input").value;
+  //check if input is empty
+  if (!city) {
+    alert("Please enter a city");
+    return;
+  }
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(currentWeatherUrl)
+    .then((response) => response.json())
+    .then((data) => displayWeather(data))
+    .catch((error) => {
+      console.error("Error fetching current weather data:", error);
+      alert("Error fetching current data, Please try again");
+    });
+}
+
+// Call the fetchWeather function after getting the input value
 fetchWeather();
